f555ca2c38efd4072cbb6ce4532e85f0fb348631
grongierisc <guillaume.rongier@intersystems.com>
2024-09-09 12:41:53 +0200

    feat: add hl7v2.py script to render ORU-R01 messages

--- scripts/hl7v2.py
+++ scripts/hl7v2.py
@@ -1,12 +1,5 @@
-from pathlib import Path
-from liquid import FileExtensionLoader
-from fhir_converter.renderers import Hl7v2Renderer, make_environment, hl7v2_default_loader
+from fhir_converter.renderers import Hl7v2Renderer
 
-templates_dir = Path("fhir_converter/templates/hl7v2")
-
-with open("data/sample/hl7v2/ADT-A01-01.hl7",mode="r",encoding="utf-8") as hl7v2_in:
-    # env=make_environment(
-    #     loader=FileExtensionLoader(search_path=templates_dir),
-    #     additional_loaders=[hl7v2_default_loader],
-    # )
-    print(Hl7v2Renderer().render_fhir_string("ADT_A01_test", hl7v2_in))
+with open("data/sample/hl7v2/ORU-R01-01.hl7",mode="r",encoding="utf-8") as hl7v2_in:
+    result = Hl7v2Renderer().render_fhir_string("ORU_R01", hl7v2_in)
+    print(result)
